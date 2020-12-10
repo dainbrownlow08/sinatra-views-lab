58928cc58814406964bceac5d72c1da0976b3b03
dainbrownlow08 <dain.brownlow@outlook.com>
2020-12-10 11:13:54 -0800

    go

--- app.rb
+++ app.rb
@@ -4,5 +4,19 @@ class App < Sinatra::Base
 		erb :index
 	end
 
+	get '/hello' do
+		erb :hello
+	end
+
+	get '/goodbye' do
+		@name = 'Joe'
+		erb :goodbye
+	end
+
+	get '/date' do
+		@date = Time.now
+    	@date_formatted = @date.strftime('%A, %B %d, %Y')
+		erb :date
+	end
 
 end
